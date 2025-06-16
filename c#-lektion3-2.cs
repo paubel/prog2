0d726f547c1135228448357115d6be31d389b998
Paul Belfrage <paul.belfrage@gmail.com>
2025-06-16 10:46:36 +0200

    Add files via upload

---
+++ c#-lektion3-2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Book
+{
+    public string Title;
+    public string Author;
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"{Title} av {Author}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Skapa ett objekt av klassen Book
+        Book book1 = new Book("1984", "George Orwell");
+
+        // Anropa metoden som skriver ut information
+        book1.PrintInfo();
+
+        // Du kan skapa fler böcker om du vill
+        Book book2 = new Book("Brave New World", "Aldous Huxley");
+        book2.PrintInfo();
+    }
+}
