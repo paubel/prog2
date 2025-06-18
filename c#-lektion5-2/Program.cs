518fd8dee46674f0445b80bdf62c40f9e37150ba
Paul Belfrage <paul.belfrage@gmail.com>
2025-06-18 10:52:56 +0200

    Add files via upload

---
+++ c#-lektion5-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Databas.SkapaDatabas();
+
+        while (true)
+        {
+            Console.WriteLine("\nMeny:");
+            Console.WriteLine("1. Lägg till post");
+            Console.WriteLine("2. Visa alla");
+            Console.WriteLine("3. Sök post");
+            Console.WriteLine("4. Avsluta");
+            Console.Write("Välj: ");
+            var val = Console.ReadLine();
+            switch (val)
+            {
+                case "1": Databas.LaggTill(); break;
+                case "2": Databas.VisaAlla(); break;
+                case "3": Databas.Sok(); break;
+                case "4": return;
+                default: Console.WriteLine("Ogiltigt val."); break;
+            }
+        }
+    }
+}
