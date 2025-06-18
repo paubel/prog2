7d90a5c748b690ff26a65e63b83597ff07e7ae20
Paul Belfrage <paul.belfrage@gmail.com>
2025-06-18 10:28:55 +0200

    Add files via upload

---
+++ c#-lektion4-3-player.cs
@@ -0,0 +1,12 @@
+﻿using RPG.Spelare;
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var p = new Player { Namn = "Ella" };
+        Console.WriteLine($"Spelaren heter: {p.Namn}");
+    }
+}
